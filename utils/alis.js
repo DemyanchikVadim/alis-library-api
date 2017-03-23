d68289f639fe5feb75551e81edd08f1803c93631
Demyanchik Vadim <vadim.demyanchik@gmail.com>
2017-03-23 14:08:06 +0300

    Add jar to enable cookies

--- utils/alis.js
+++ utils/alis.js
@@ -1,32 +1,22 @@
-import request from 'request';
-import cheerio from 'cheerio';
+const cheerio = require('cheerio');
+const request = require('request').defaults({ jar: true });
 
 function alis(year) {
-  request(`http://86.57.174.45/alis/EK/do_searh.php?radiodate=simple&valueINP=${year}&tema=ALL&tag=ALL`, (error, response, html) => {
+  const url = `http://86.57.174.45/alis/EK/do_searh.php?radiodate=simple&valueINP=${year}&tema=ALL&tag=ALL`;
+  request(url, (error, response, html) => {
     if (!error) {
       const $ = cheerio.load(html);
-      const links = $('a[href^=\'do_other\']');
-      const urls = [];
-      $(links).each((i, link) => {
-        urls.push(`${$(link).attr('href')}`);
-      });
+      const pageLinks = $('a[href^=\'do_other\']');
+      const pageUrls = $(pageLinks).map((i, link) => $(link).attr('href')).toArray();
 
       const books = [];
-      urls.forEach((elem, i) => {
-        request(`http://86.57.174.45/alis/EK/${urls[i]}`, (err, res, body) => {
-          if (!err) {
-            const $ = cheerio.load(body);
-            $('.article').each(function () {
-              books.push($(this).text());
-            });
-            console.log(books.length);
-          }
-        });
+      $('.article').each(function () {
+        books.push($(this).text());
       });
+    } else {
+      console.log(error);
     }
   });
 }
-
-alis(2013);
 
 export default alis;
