047079e08f3e56ce13887c99978533b979b2ba86
Demyanchik Vadim <vadim.demyanchik@gmail.com>
2017-03-27 13:37:05 +0300

    Rename alis to scrapeAlis

--- consumer.js
+++ consumer.js
@@ -1,3 +1,5 @@
-import alis from './utils/alis';
+import scrapeAlis from './utils/alis';
 
-alis(2013);
+/* i how consumer alis script, want to get all book 2013 year and then show them in console.log,
+in the future i want to save books in database */
+scrapeAlis(2013).then(books => console.log(books));
