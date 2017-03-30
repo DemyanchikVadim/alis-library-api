f124a0c49ade9cdcb7e5df38a2ea920037689e47
Demyanchik Vadim <vadim.demyanchik@gmail.com>
2017-03-30 11:29:31 +0300

    Replace promise interface

--- utils/alis.js
+++ utils/alis.js
@@ -1,112 +1,44 @@
-// cheerio for parse html pages
 const cheerio = require('cheerio');
-// jar for create cookie
 const request = require('request').defaults({ jar: true });
 
-// get AllNextPageUrls recursive
-function getAllNextUrls(url) {
-  const promise = new Promise((resolve, reject) => {
-    // array allNextPagesUrls
-    const allNextPagesUrls = [];
-    // quit recursion
-    if (url === 'undefined') return;
-    // make request, here need use async or promise
-    request(`http://86.57.174.45/alis/EK/${url}`, (err, res, body) => {
-      if (!err) {
-        // load html
-        const $ = cheerio.load(body);
-        // get next url page
-        const nextPageLink = $('a[id^=\'Agt\']');
-        const nextPageUrl = `${$(nextPageLink).attr('href')}`;
-        if (nextPageUrl === 'undefined') {
-          getAllNextUrls(nextPageUrl);
-        } else {
-          // make recursive request with next url and push url in array
-          getAllNextUrls(nextPageUrl);
-          allNextPagesUrls.push(nextPageUrl);
-        }
-      }
-    });
-    if (allNextPagesUrls.length) {
-      resolve(allNextPagesUrls);
-    } else {
-      reject('Error');
-    }
-  });
-  return promise;
-}
-// get all books 2013 years by pageUrls array
-function getAllBooks(pageUrls) {
-  const promise = new Promise((resolve, reject) => {
-    // array all 2013 year books
-    // for each url make request and scrape books
-    Promise.all(pageUrls.map(url => new Promise((resolve, reject) => {
-      request(`http://86.57.174.45/alis/EK/${url}`, (err, resp, body) => {
-        if (err) {
-          return reject(err);
-        }
-        const $ = cheerio.load(body);
-        const books = [];
-        // get all books on the page
-        $('.article').each(function () {
-          books.push($(this).text());
-        });
-        console.log(books);
-        resolve(books);
-      });
-    }))).then((books) => {
-      resolve(books);
-    }).catch((err) => {
+const START_URL = 'http://86.57.174.45/alis/EK/do_searh.php?radiodate=simple&valueINP=2016&tema=ALL&tag=ALL';
+
+function getPage(url, callback) {
+  request(url, (err, response, body) => {
+    if (err) {
       console.log(err);
-    });
-
-    if (!pageUrls.length) {
-      reject('Error');
+      return;
     }
+    callback(null, body);
   });
-  return promise;
 }
 
-// make get request on alis library to get all books 2013 year
-function scrapeAlis(query) {
-  // make promise
-  const promise = new Promise((resolve, reject) => {
-    // url consumer defined, in our case 2013 year
-    const url = `http://86.57.174.45/alis/EK/do_searh.php?radiodate=simple&valueINP=${query}&tema=ALL&tag=ALL`;
-    // make request
-    request(url, (error, response, html) => {
-      if (!error) {
-        // load main html page
-        const $ = cheerio.load(html);
-        // scrape first page to get first ten url in pagination, then save urls in array
-        const pageLinks = $('a[href^=\'do_other\']');
-        const pageUrls = $(pageLinks).map((i, link) => $(link).attr('href')).toArray();
+function run(fn, q) {
+  if (q.length === 0) {
+    console.log('end of queue');
+    return;
+  }
 
-       // scrape only last url in pagination,
-        const nextPageLink = $('#Agt');
-        const nextPageUrl = (`${$(nextPageLink).attr('href')}`);
-
-        // make request on nextPageUrl, for get allNextPagesUrls recursive,
-        // then combine pageUrls and allNextPageUrls arrays
-        getAllNextUrls(nextPageUrl)
-          .then((allNextPagesUrls) => {
-            Array.prototype.push.apply(pageUrls, allNextPagesUrls);
-          })
-          .catch(err => console.log(err));
-
-        // get all books 2013 year and send them to consumer
-        getAllBooks(pageUrls)
-          .then(books => resolve(books))
-          .catch(err => console.log(err));
-      } else {
-        console.log(error);
-      }
-    });
-    if (query === '') {
-      reject('Error: bad request');
+  console.log(`running fn for ${q[0]}`);
+  fn(q[0], (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
     }
+    const $ = cheerio.load(data);
+    const nextPageLink = $('#Agt');
+    const nextPageUrl = (`${$(nextPageLink).attr('href')}`);
+    const remainingQueue = q.slice(1);
+
+    if (nextPageUrl !== 'undefined') {
+      remainingQueue.push(`http://86.57.174.45/alis/EK/${nextPageUrl}`);
+    } else {
+      console.log('end of queue');
+      return;
+    }
+
+    run(fn, remainingQueue);
   });
-  return promise;
 }
 
-export default scrapeAlis;
+run(getPage, [START_URL]);
